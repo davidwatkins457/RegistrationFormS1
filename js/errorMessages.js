3616433668ef264e207126f9ddf09d13ea6556b1
David Watkins <david.watkins457@west-mec.org>
2016-10-28 14:53:31 -0700

    no message

--- js/errorMessages.js
+++ js/errorMessages.js
@@ -5,7 +5,39 @@
 // It takes two arguments: the form element ID and the message.
 function addErrorMessage(id, msg) {
    	'use strict';
-    
+
+	//Get a reference to the erred input
+	var elem = U.$(id);
+
+	//Create a new id for a span for the error
+	var newId = id + "Error";
+	//i.e firstNameError for firstName
+
+	//Span might already exist
+	var span = U.$(newId);
+	if	(span) {
+		span.firstChild.value = msg;
+	}
+	else {
+		//span doesn't exist, add it:
+		span = document.createElement("span");
+		span.id = newId;
+		span.className = "error";
+		span.appendChild(
+			document.createTextNode(msg)
+		);
+
+        //Add the span to the parent control:
+        //to the RIGHT of the input box
+        elem.parentNode.appendChild(span);
+        //and to the left of the input box,
+        //update the label:
+        //elem.parentNode.childNodes[1].className = "error";
+		elem.previousSibling.className = "error";
+	}
+
+
+
 } // End of addErrorMessage() function.
 
 // This function removes the error message.
@@ -13,4 +45,20 @@ function addErrorMessage(id, msg) {
 function removeErrorMessage(id) {
    	'use strict';
 
+    //Removes error message IF it already exists.
+    //Does nothing if there is no error message
+
+    var span = U.$(id + "Error");
+
+    if (span){
+        //Remove red text from the label,
+        //aka the sibling of the sibling (input):
+        //span.previousSibling.previousSibling.className = null;
+		span.parentNode.childNodes[1].className= null;
+        //Remove the span completely
+        span.parentNode.removeChild(span);
+        //The parent of the span is a div tag
+        //So remove the child span tag from
+        //the parent
+    }
 } // End of removeErrorMessage() function.
--- js/register.js
+++ js/register.js
@@ -4,20 +4,197 @@
 // Function called when the form is submitted.
 // Function validates the form data.
 
+function validateUsername(username, message){
+    //Validates that a username meets
+    //the following criteria:
+    //1. Must be at least 8 characters long
+    //2. First character must be A-Z or a-z
+    //3. Must contain at least one digit (0-9)
+    //Function returns "true" if all criteria
+    // is met, "false" if any criteria is not met.
+
+    var char1;
+    var hasNumber;
+
+    //Check username length;
+    if (username.length < 8) {
+        message.valueOf = "User name must be at least 8 characters";
+        return false;
+    }
+
+    //Check first character:
+    char1 = username.substr(0,1).toUpperCase();
+    if (!(char1 >= "A" && char1 <= "Z")) {
+        message.valueOf = "First character must be A-Z or a-z";
+        return false;
+    }
+
+
+    //Check for at least 1 digit/numeral:
+    hasNumber = /\d/;
+    if (!(hasNumber.test(username))){
+        message.valueOf = "USer name must contain at least one numeral";
+        return false;
+    }
+    /*
+    //Alternate version:
+    var anyDigits = false;
+    while (!(anyDigits)){
+        for (var i = 1; i < username.length; i++)
+        {
+            char1 = userName.substr(i,1);
+            if (char1 >= "0" && char1 <= "9")
+            {
+                anyDigits = true;
+                break; //exit for loop, found one!
+            } //end if
+        } //end for
+        if(!(anyDigits)){
+            return false;
+        }
+    }// end while
+
+    //Otherwise, all criteria met:
+    return true;
+*/
+} //end function
+
 function validateForm(e) {
     'use strict';
-    
+
+    if (typeof e == 'undefined') {
+        //This is a browser window-generated event
+        e = window.event;
+    }
+
+    //Get form references:
+    var firstName = U.$('firstName');
+    var lastName = U.$('lastName');
+    var userName = U.$('userName');
+    var email = U.$('email');
+    var phone = U.$('phone');
+    var city = U.$('city');
+    var state = U.$('state');
+    var zip = U.$('zip');
+    var terms = U.$('terms');
+
+    //error flag:
+    var error = false;
+
+    //Validate the first name using a
+    //regular expression:
+    if (/^[A-Z .\-']{2,20}$/i.test(firstName.value)) {
+        // Everything between / and / is the expression
+        //Any letter A-Z (case insensitive) is valid
+        //Spaces, periods, and hyphens are valid
+        //name must be 2 - 20 characters long
+
+        //First name matches the expression
+        //and is valid
+        //alert("Valid first name");
+        removeErrorMessage('firstName');
+    }
+    else {
+        //alert("Invalid first name");
+        addErrorMessage('firstName',
+            'Invalid/missing first name');
+        error = true;
+    }
+    if (/^[A-Z .\-']{2,20}$/i.test(lastName.value)) {
+        // Everything between / and / is the expression
+        //Any letter A-Z (case insensitive) is valid
+        //Spaces, periods, and hyphens are valid
+        //name must be 2 - 20 characters long
+
+        //First name matches the expression
+        //and is valid
+        //alert("Valid first name");
+        removeErrorMessage('lastName');
+    }
+    else {
+        //alert("Invalid last name");
+        addErrorMessage('lastName',
+            'Invalid/missing last name');
+        error = true;
+    }
+
+    var msg = "initial message";
+    msg = Object(msg);
+    if(validateUsername(userName.value, msg)){
+        //The username meets requirements
+        removeErrorMessage('userName');
+    }
+    else{
+        //The username is not valid
+        addErrorMessage('userName',msg.valueOf);
+        error = true;
+    }
+
+    if (error) {
+        if (e.preventDefault) {
+            e.preventDefault();
+        }
+        else {
+            e.returnValue = false;
+        }
+    }
+    //The email
+    if (/^[\w.-]+@[\w.-]+\.[A-Za-z]{2,6}$/i.test(email.value)) {
+        // w = any alphanumeric character
+        //A-Za-z = any alphabetic character
+        //2-6 = how many characters are required
+
+        //First name matches the expression
+        //and is valid
+        //alert("Valid first name");
+        removeErrorMessage('email');
+    }
+    else {
+        //alert("Invalid first name");
+        addErrorMessage('email',
+            'Invalid/missing email');
+        error = true;
+    }
+
+    //Phone number
+    if (/^\d{3}[ \-.]?\d{3}[ \-.]?\d{4}$/i.test(phone.value)){
+        removeErrorMessage('phone');
+    }
+    else {
+        addErrorMessage('phone','(Should be in the format xxx-xxx-xxxx or xxx.xxx.xxxx)');
+    }
+
+    //Zip code:
+    if (/^\d{5}(\-\d{4})?$/i.test(zip.value)){
+        removeErrorMessage('zip');
+    }
+    else{
+        addErrorMessage('zip','(Should be in the format xxxxx-xxxx)');
+    }
+
+    return false;
 } // End of validateForm() function.
 
 // Function called when the terms checkbox changes.
 // Function enables and disables the submit button.
-function toggleSubmit() {
-	'use strict';
-    
+    function toggleSubmit() {
+    'use strict';
+
 } // End of toggleSubmit() function.
 
 // Establish functionality on window load:
 window.onload = function() {
     'use strict';
+
+    //addEvent requires three pieces of info:
+    //1. The object to add the event listener to
+    //2. The event to listen for
+    //3. The function to call in response to
+    //  the event in #2
+    U.addEvent(
+        U.$('theForm'),
+        'submit',
+        validateForm);
+
 
 };
